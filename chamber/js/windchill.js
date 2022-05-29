df7a1c82ff6ba1300e1e430c74b0ecba5078db75
marcosbarrozo <bar22007@byui.edu>
2022-05-28 22:32:38 -0300

    responsive design

---
+++ chamber/js/windchill.js
@@ -0,0 +1,15 @@
+function windChill (temperature, windChill){
+	
+	return 35.74 + 0.6215*temperature - 35.75*Math.pow(windChill,0.16) + 0.4275*temperature*Math.pow(windChill,0.16)
+
+}
+
+const  temperature = parseFloat(document.querySelector("#temperature").innerText)
+const windSpeed = parseFloat(document.querySelector("#wind-speed").innerText);
+const windChillValue = document.querySelector("#wind-chill");
+
+if(temperature <= 50 && windSpeed > 3){
+    windChillValue.innerText = windChill(temperature,windSpeed).toFixed(1);
+} else {
+    windChillValue.innerText = "N/A";
+}
